96f9bb91a4fb6b3e74d793464f09205f7aae207b
Deemo Chen <yc938@cornell.edu>
2023-12-01 16:45:55 -0500

    possible features to disable

--- lib/ekf/ekf.h
+++ lib/ekf/ekf.h
@@ -2,6 +2,10 @@
 // However, this should not be done prematurely, as it also hides otherwise hard-to-find programming mistakes (such as combining expressions of incompatible dimensions)!
 // Disable EIGEN DEBUGGING!!!
 #define NDEBUG
+// #define EIGEN_NO_DEBUG
+// #define EIGEN_NO_MALLOC
+// #define EIGEN_NO_STL
+// #define EIGEN_MPL2_ONLY
 #include <ArduinoEigenDense.h>
 
 class EKF
@@ -12,8 +16,8 @@ public:
     void initialize(double delta_t, const Eigen::VectorXd &initial_state, const Eigen::MatrixXd &initial_covariance, const Eigen::MatrixXd &process_noise_covariance, const Eigen::MatrixXd &noise_covariance, const Eigen::MatrixXd &Hd);
     void step();
 
-    Eigen::VectorXd state;
-    Eigen::VectorXd Z;
+    Eigen::Matrix<double, 6, 1> state;
+    Eigen::Matrix<double, 6, 1> Z;
     Eigen::MatrixXd covariance;
 
 private:
